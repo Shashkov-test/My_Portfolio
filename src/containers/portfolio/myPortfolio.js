415a85822d106fdfca90b0f01d2fb64ef0335e5e
Victor Shashkov <liar0qq@gmail.com>
2023-04-18 16:38:03 +0300

    Update myPortfolio.js

--- src/containers/portfolio/myPortfolio.js
+++ src/containers/portfolio/myPortfolio.js
@@ -1,58 +1,66 @@
-import ImageMarvel from '../../images/marvel.png';
-import ImageToDoList from '../../images/todolist.png';
-import ImageNothing from '../../images/nothing.png';
-import ImageCryptoverse from '../../images/cryptoverse.png'
+import ImageMarvel from "../../images/marvel1.png";
+import ImageToDoList from "../../images/td.png";
+import ImageNothing from "../../images/nothing.png";
+import ImageCryptoverse from "../../images/cryptoverse.png";
+import ImageHeroes from "../../images/heroes.png";
 
 export const portfolioData = [
-    {
-      id: 2,
-      name: "Cryptoverse",
-      image: ImageCryptoverse,
-      link: 'https://cryptoverce-shashkov.netlify.app/',
-    },
+  {
+    id: 2,
+    name: "Marvel Information Portal",
+    image: ImageMarvel,
+    link: "https://shashkov-marvel-information-portal.netlify.app/",
+  },
 
-    {
-      id: 2,
-      name: "Marvel Website",
-      image: ImageMarvel,
-      link: 'https://shashkov-test.github.io/marveltest/',
-    },
-  
-    {
-      id: 2,
-      name: "TodoList",
-      image: ImageToDoList,
-      link: 'https://shashkov-todo-list.netlify.app/',
-    },
-  
-    {
-      id: 3,
-      name: "Nothing here.....",
-      image: ImageNothing,
-      link: 'http://www.thereisnothinghere.com/',
-    },
+  {
+    id: 2,
+    name: "Cryptoverse",
+    image: ImageCryptoverse,
+    link: "https://cryptoverce-shashkov.netlify.app/",
+  },
 
-    /* {
+  {
+    id: 2,
+    name: "Hero admin panel template",
+    image: ImageHeroes,
+    link: "https://shashkov-hero-admin-panel-template.netlify.app/",
+  },
+
+  {
+    id: 2,
+    name: "TodoList",
+    image: ImageToDoList,
+    link: "https://shashkov-todo-list.netlify.app/",
+  },
+
+  {
+    id: 3,
+    name: "Nothing here.....",
+    image: ImageNothing,
+    link: "http://www.thereisnothinghere.com/",
+  },
+
+  /* {
       id: 2,
       name: "Random online store",
       image: ImageNothing,
       link: 'http://www.thereisnothinghere.com/',
     }, */
 ];
-  
+
 export const filterData = [
-    {
-        filterId: 1,
-        label: "All"
-    },
+  {
+    filterId: 1,
+    label: "All",
+  },
 
-    {
-        filterId: 2,
-        label: "Development"
-    },
+  {
+    filterId: 2,
+    label: "Development",
+  },
 
-    {
-        filterId: 3,
-        label: "Something else"
-    },
+  {
+    filterId: 3,
+    label: "Something else",
+  },
 ];
